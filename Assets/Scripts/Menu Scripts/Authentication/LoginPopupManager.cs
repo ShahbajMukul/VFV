56d893cc5587a43d7f656442282e82b9c85ed352
Shahbaj Mukul <shahbajmukul@gmail.com>
2024-10-11 19:30:30 -0400

    Regi + Login UI added

---
+++ Assets/Scripts/Menu Scripts/Authentication/LoginPopupManager.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class LoginPopupManager : MonoBehaviour
+{
+    public GameObject loginPopup;
+    public GameObject RegistrationPopup;
+    // add reset password parameter later
+    public InputField emailInput;
+    public InputField passwordInput;
+    public UnityEngine.UI.Text errorMessageText;
+
+    public void OnLoginButtonClicked()
+    {
+        if (string.IsNullOrEmpty(emailInput.text) ||
+            string.IsNullOrEmpty(passwordInput.text  ))
+        {
+            errorMessageText.text = "All fields must be filled!";
+            return;
+        }
+
+        // ToDo: Add Login api call
+        UnityEngine.Debug.Log("Login successfull!");
+        CloseLoginPopup();
+    }
+
+    public void CloseLoginPopup()
+    {
+        loginPopup.SetActive(false);
+    }
+
+    public void ShowRegiPopup()
+    {
+        CloseLoginPopup();
+        RegistrationPopup.SetActive(true);
+    }
+
+    public void ShowResetPopup()
+    {
+        // hide login popup
+        // Show Reset popup later 
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/Authentication/RegistrationPopupManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class RegistrationPopupManager : MonoBehaviour
+{
+    public GameObject registrationPopup;
+    public GameObject loginPopup;
+    public InputField emailInput;
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public InputField confirmPasswordInput;
+    public InputField registrationCodeInput;
+    public UnityEngine.UI.Text errorMessageText;
+
+    void Start()
+    {
+        ShowRegistrationPopup();
+    }
+
+    public void ShowRegistrationPopup()
+    {
+        registrationPopup.SetActive(true);
+    }
+
+    public void OnRegisterButtonClicked()
+    {
+        // Validation
+        if (string.IsNullOrEmpty(emailInput.text) ||
+            string.IsNullOrEmpty(usernameInput.text) ||
+            string.IsNullOrEmpty(passwordInput.text) ||
+            string.IsNullOrEmpty(confirmPasswordInput.text) ||
+            string.IsNullOrEmpty(registrationCodeInput.text))
+        {
+            errorMessageText.text = "All fields must be filled!";
+            return;
+        }
+
+        if (passwordInput.text != confirmPasswordInput.text)
+        {
+            errorMessageText.text = "Passwords do not match!";
+            return;
+        }
+
+        // ToDo: Add backend register api call here
+
+        UnityEngine.Debug.Log("Registration successful!");
+        CloseRegistrationPopup();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        CloseRegistrationPopup();
+    }
+
+    void CloseRegistrationPopup()
+    {
+        registrationPopup.SetActive(false);
+    }
+
+    public void ShowLoginPopup()
+    {
+        CloseRegistrationPopup();
+        loginPopup.SetActive(true);
+    }
+}
