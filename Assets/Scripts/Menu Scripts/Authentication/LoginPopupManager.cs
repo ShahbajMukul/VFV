8fe01524114f82d99f0b0d50c2ece9a092f150f1
Abdullah Mahith <abdmahith91@gmail.com>
2024-10-17 14:05:39 -0400

    registration connection test

--- Assets/Scripts/Menu Scripts/Authentication/LoginPopupManager.cs
+++ Assets/Scripts/Menu Scripts/Authentication/LoginPopupManager.cs
@@ -1,31 +1,60 @@
-﻿using System.CodeDom;
-using System.Collections;
-using System.Collections.Generic;
+﻿/*using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 
 public class LoginPopupManager : MonoBehaviour
 {
     public GameObject loginPopup;
-    public GameObject RegistrationPopup;
-    // add reset password parameter later
+    public GameObject registrationPopup;
     public InputField emailInput;
     public InputField passwordInput;
     public UnityEngine.UI.Text errorMessageText;
 
+    private string loginUrl = "http://localhost:3000/api/login";
+
     public void OnLoginButtonClicked()
     {
-        if (string.IsNullOrEmpty(emailInput.text) ||
-            string.IsNullOrEmpty(passwordInput.text  ))
+        if (string.IsNullOrEmpty(emailInput.text) || string.IsNullOrEmpty(passwordInput.text))
         {
             errorMessageText.text = "All fields must be filled!";
             return;
         }
 
-        // ToDo: Add Login api call
-        UnityEngine.Debug.Log("Login successfull!");
-        CloseLoginPopup();
+        // Start the login coroutine
+        StartCoroutine(LoginUser(emailInput.text, passwordInput.text));
+    }
+
+    private IEnumerator LoginUser(string email, string password)
+    {
+        // Create the JSON payload manually
+        string jsonData = JsonUtility.ToJson(new
+        {
+            username = email,  // The backend uses 'username' field for login (email in your case)
+            password = password
+        });
+
+        // Create a UnityWebRequest to POST the JSON data
+        UnityWebRequest request = new UnityWebRequest(loginUrl, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogError("Login failed: " + request.downloadHandler.text);
+            errorMessageText.text = "Login failed: " + request.error;
+        }
+        else
+        {
+            Debug.Log("Login successful!");
+            errorMessageText.text = "";
+            CloseLoginPopup();
+        }
     }
 
     public void CloseLoginPopup()
@@ -36,12 +65,13 @@ public class LoginPopupManager : MonoBehaviour
     public void ShowRegiPopup()
     {
         CloseLoginPopup();
-        RegistrationPopup.SetActive(true);
+        registrationPopup.SetActive(true);
     }
 
     public void ShowResetPopup()
     {
-        // hide login popup
+        // Hide login popup
         // Show Reset popup later 
     }
 }
+*/
--- Assets/Scripts/Menu Scripts/Authentication/RegistrationPopupManager.cs
+++ Assets/Scripts/Menu Scripts/Authentication/RegistrationPopupManager.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using UnityEngine.Networking;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 
 public class RegistrationPopupManager : MonoBehaviour
 {
@@ -15,6 +15,9 @@ public class RegistrationPopupManager : MonoBehaviour
     public InputField confirmPasswordInput;
     public InputField registrationCodeInput;
     public UnityEngine.UI.Text errorMessageText;
+
+    private string registrationUrl = "http://localhost:3000/api/register"; 
+
 
     void Start()
     {
@@ -45,10 +48,35 @@ public class RegistrationPopupManager : MonoBehaviour
             return;
         }
 
-        // ToDo: Add backend register api call here
+        // Debug log to check email and password before sending request
+        UnityEngine.Debug.Log($"Attempting to register with email: {emailInput.text}, password: {passwordInput.text}");
 
-        UnityEngine.Debug.Log("Registration successful!");
-        CloseRegistrationPopup();
+        StartCoroutine(RegisterUser(emailInput.text, usernameInput.text, passwordInput.text, registrationCodeInput.text));
+    }
+
+    private IEnumerator RegisterUser(string email, string username, string password, string registrationCode)
+    {
+        
+        WWWForm form = new WWWForm();
+        form.AddField("email", email);
+        form.AddField("username", username);
+        form.AddField("password", password);
+        form.AddField("secretCode", registrationCode);
+
+        UnityWebRequest request = UnityWebRequest.Post(registrationUrl, form);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            errorMessageText.text = "Registration failed: " + request.error;
+            UnityEngine.Debug.LogError("Registration failed: " + request.downloadHandler.text);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Registration successful!");
+            CloseRegistrationPopup();
+            ShowLoginPopup();
+        }
     }
 
     public void OnCancelButtonClicked()
