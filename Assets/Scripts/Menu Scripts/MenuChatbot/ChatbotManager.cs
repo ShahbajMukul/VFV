1ae35d5e911d485e6b58570e2250532ee8cc6b21
Shahbaj Mukul <shahbajmukul@gmail.com>
2024-10-24 14:50:16 -0400

    Chatbot History bar WIP

--- Assets/Scripts/Menu Scripts/MenuChatbot/ChatbotManager.cs
+++ Assets/Scripts/Menu Scripts/MenuChatbot/ChatbotManager.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using static System.Net.Mime.MediaTypeNames;
 using UnityEngine.UI;
+using System.Security.Cryptography;
 public class ChatbotManager : MonoBehaviour
 {
     public GameObject chatbotPopup;
     public GameObject messagePrefab;  
     public Transform chatContent;     
     public InputField chatInputField; 
-    public ScrollRect chatScrollView; 
+    public ScrollRect chatScrollView;
+
+    public Transform chatHistoryContent;
+    public GameObject historyItemPrefab;
+
+    // for development
+    private List<List<string>> chatSessions = new List<List<string>>();
+    private List<string> currentSessions = new List<string>();
+
+    void Start()
+    {
+        LoadChatHistory();
+    }
+
 
 
     public void OnSendButtonClicked()
@@ -71,6 +85,19 @@ public class ChatbotManager : MonoBehaviour
         }
     }
 
+    public void OnNewChatButtonClicked()
+    {
+        // Delete the messages in the display panel
+        foreach (Transform child in chatContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        currentSessions.Clear();
+
+        LoadChatHistory();
+    }
+
     public void OpenChatbotPopUp()
     {
         chatbotPopup.SetActive(true);
@@ -79,5 +106,52 @@ public class ChatbotManager : MonoBehaviour
     public void CloseChatbotPopUp()
     {
         chatbotPopup.gameObject.SetActive(false);
+
+        // For testing, no need after API implementation
+        if (currentSessions.Count > 0)
+        {
+            chatSessions.Add(new List<string>(currentSessions) );
+            currentSessions.Clear();
+            LoadChatHistory();
+        }
     }
+
+    void LoadChatHistory()
+    {
+        foreach (Transform child in chatHistoryContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Populate history panel with API call
+        for (int i = 0; i < chatSessions.Count; i++)
+        {
+            int sessionIndex = i;
+            GameObject newHistoryItem = Instantiate(historyItemPrefab, chatHistoryContent);
+            UnityEngine.UI.Text historyText = newHistoryItem.GetComponentInChildren<UnityEngine.UI.Text>();
+            historyText.text = "Session " + (i + 1);
+
+            // Add click event to load the session
+            Button historyButton = newHistoryItem.GetComponent<Button>();
+            historyButton.onClick.AddListener(() => LoadChatSession(sessionIndex));
+        }
+    }
+
+    void LoadChatSession(int sessionIndex)
+    {
+        // Clear current chat display
+        foreach (Transform child in chatContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Use API call
+        List<string> selectedSession = chatSessions[sessionIndex];
+        foreach (string message in selectedSession)
+        {
+            bool isUser = message.StartsWith("User: ");
+            DisplayMessage(message.Replace("User: ", "").Replace("AI: ", ""), isUser);
+        }
+    }
+
 }
