8eb41e3c3957866e30b45859bf14c06cc6a9467e
Abdullah Mahith <abdmahith91@gmail.com>
2025-01-16 15:41:57 -0500

    Skip Button

---
+++ Assets/Scripts/SkipButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipButton : MonoBehaviour
+{
+    public string sceneName = "NewMainMenu";
+
+    public void OnSkipButtonClicked()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
