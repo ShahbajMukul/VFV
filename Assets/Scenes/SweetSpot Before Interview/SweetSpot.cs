689fa91e248bedb4a9286e8105ffac6067ece56b
Abdullah Mahith <abdmahith91@gmail.com>
2025-02-09 20:40:02 -0500

    Sweet Spot Implementation WIP
    -adding the sweet spot UI separately to each interview scenes.

---
+++ Assets/Scenes/SweetSpot Before Interview/SweetSpot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SweetSpot : MonoBehaviour
+{
+    public void OpenStorAI()
+    {
+        Application.OpenURL("https://storai.net/dashboard");
+        Time.timeScale = 0;
+    }
+
+    public void ShowSweetSpotUI()
+    {
+        gameObject.SetActive(true); 
+        Time.timeScale = 0; 
+    }
+
+    public void HideSweetSpotUI()
+    {
+        transform.parent.gameObject.SetActive(false); 
+
+    }
+}
