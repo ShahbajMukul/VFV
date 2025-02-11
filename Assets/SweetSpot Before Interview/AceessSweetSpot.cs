764c82643d7bbfc0e87d51450c6dcdf850e35fb0
Abdullah Mahith <abdmahith91@gmail.com>
2025-02-10 19:39:51 -0500

    updated SweetSpot

--- Assets/SweetSpot Before Interview/AceessSweetSpot.cs
+++ Assets/SweetSpot Before Interview/AceessSweetSpot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AceessSweetSpot : MonoBehaviour
-{
+{   //open the storAi modelu site 
     public void AccessSweetSpot()
     {
         Application.OpenURL("https://storai.net/modules");
